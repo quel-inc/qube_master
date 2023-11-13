ec7675f4e750f70c3c2009233f85d610d8fb08e8
Takefumi MIYOSHI <miyo@wasamon.net>
2023-11-13 10:40:46 +0900

    add a new entry for 0e213d.mcs

--- create_mcs.tcl
+++ create_mcs.tcl
@@ -5,3 +5,4 @@ write_cfgmem -force -format mcs -size 128 -interface SPIx4 -loadbit {up 0x010020
 write_cfgmem -force -format mcs -size 128 -interface SPIx4 -loadbit {up 0x01002000 "bin/0e1cb8.bit" } -file "bin/0e1cb8.mcs"
 write_cfgmem -force -format mcs -size 128 -interface SPIx4 -loadbit {up 0x01002000 "bin/0e1cc0.bit" } -file "bin/0e1cc0.mcs"
 write_cfgmem -force -format mcs -size 128 -interface SPIx4 -loadbit {up 0x01002000 "bin/0d50a4.bit" } -file "bin/0d50a4.mcs"
+write_cfgmem -force -format mcs -size 128 -interface SPIx4 -loadbit {up 0x01002000 "bin/0e213e.bit" } -file "bin/0e213e.mcs"
